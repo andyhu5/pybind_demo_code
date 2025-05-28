573abdaf7cc5a96244528b515daf057ed80b4e75
Your Name <you@example.com>
2025-05-28 15:28:10 +0800

    init

---
+++ testCase2.py
@@ -0,0 +1,8 @@
+from build.module_name import * # type: ignore
+
+print(some_fn_python_name(5.0,3.0)) # type: ignore
+print(my_add(15.0,3.0)) # type: ignore
+
+print(Foo([1.0, 2.0, 3.0])) # type: ignore
+
+print(Bar((5.0, 10.0))) # type: ignore
